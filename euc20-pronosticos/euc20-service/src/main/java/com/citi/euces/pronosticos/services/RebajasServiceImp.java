8eac2b292e2680884efbece623f1a7cff3fa3ba5
Marlene Gutierrez <>
2021-06-25 15:59:18 -0500

    Valid file

--- euc20-pronosticos/euc20-service/src/main/java/com/citi/euces/pronosticos/services/RebajasServiceImp.java
+++ euc20-pronosticos/euc20-service/src/main/java/com/citi/euces/pronosticos/services/RebajasServiceImp.java
@@ -5,6 +5,7 @@ import com.citi.euces.pronosticos.infra.exceptions.GenericException;
 import com.citi.euces.pronosticos.services.api.RebajasService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +14,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -24,9 +28,30 @@ public class RebajasServiceImp implements RebajasService {
     @Override
     public MensajeDTO aplicarRebajaloadFile(String file) throws GenericException {
         log.info("aplicarRebajaloadFile ::  init");
+
+        byte[] decodedBytes = Base64.getDecoder().decode(file);
+        try {
+            FileSystemResource file1 =getFile(decodedBytes, "rebajas.zip");
+
+            log.info("file1 :: " + file1.getFilename());
+            log.info("file2 :: " + file1.getFile());
+            log.info("file3 :: " + file1.getInputStream());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         MensajeDTO response = new MensajeDTO();
         response.setMensajeConfirm("Mensaje de Confirmacion");
         response.setMensajeInfo("Mensaje Informacion");
+
         return response;
+    }
+
+
+    public FileSystemResource getFile(byte[] bos, String nameFile) throws IOException {
+        Path testFile = Files.createTempFile(nameFile + "-", ".xlsx");
+        System.out.println("Creating and Uploading Seccion File: " + testFile);
+        Files.write(testFile, bos);
+        return new FileSystemResource(testFile.toFile());
     }
 }
