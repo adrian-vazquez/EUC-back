6fea6eba87f08259584cfc89f4719cf0cfcf25d1
arturo.betanzo <arturo.betanzo@ids.com.mx>
2021-07-19 19:16:09 -0500

    Se agrega servicio GetLayout

---
+++ euc20-pronosticos/euc20-service/src/main/java/com/citi/euces/pronosticos/services/api/IRepoService.java
@@ -0,0 +1,11 @@
+package com.citi.euces.pronosticos.services.api;
+
+import com.citi.euces.pronosticos.infra.exceptions.GenericException;
+
+public interface IRepoService {
+	
+   String getLayoutProteccion()throws  GenericException;
+   
+ 
+
+}
